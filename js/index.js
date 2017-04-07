5eb8901e81643fad92e1d2cd56aa32c2abb809f4
poojab26 <pbhaumik26@gmail.com>
2017-04-08 01:01:51 +0530

    Renew

--- js/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by pblead26 on 24-Feb-17.
- */
-$(function() {
-    $("#includedContent").load("page0.html");
-});
--- js/page0-func.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by pblead26 on 04-Mar-17.
- */
-function next(){
-    $(function() {
-        $("#content").load("page1.html");
-    });
-
-}
